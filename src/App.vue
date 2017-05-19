4609f3f93618c064bde65fdfc15153bfed010cd9
biig_pongsatorn <biig_pongsatorn@hotmail.com>
2017-05-19 16:32:36 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,89 @@
+<template>
+  <div id="app">
+    <h1>facebook {{ action }} . . .</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Proxy Facebook ',
+      action: ''
+    }
+  },
+  methods: {
+    logout (uri) {
+      let vm = this
+      FB.logout(function (response) {
+        window.location = encodeURI(uri)
+      })
+    },
+    getParameterByName (name) {
+      let url = window.location.href
+      name = name.replace(/[\[\]]/g, "\\$&")
+      let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+          results = regex.exec(url)
+      if (!results) return null
+      if (!results[2]) return ''
+      return decodeURIComponent(results[2].replace(/\+/g, " "))
+    },
+    statusChangeCallback (response) {
+      let vm = this
+      let appId = 'YOUR_APP_ID'
+      let uri = encodeURI('http://localhost:8080/')
+      let redirect_uri = vm.getParameterByName('sellsuki_plugin_uri') || JSON.parse(localStorage.getItem('sellsuki_plugin_uri'))
+
+      if (vm.action === 'logout') {
+        vm.logout(redirect_uri)
+      } else {
+        if (response.status === 'connected') {
+          if (redirect_uri) {
+            if (redirect_uri == JSON.parse(localStorage.getItem('sellsuki_plugin_uri'))) {
+              localStorage.removeItem('sellsuki_plugin_uri')
+            }
+            // Send response to redirect_uri 
+            window.location = encodeURI(`${redirect_uri}?userID=${response.authResponse.userID}`)
+          } else {
+            // Handle Error
+            alert('Not have redirect uri.')
+          }
+        } else {
+          let param = vm.getParameterByName('sellsuki_plugin_uri')
+          if (param) {
+            localStorage.setItem('sellsuki_plugin_uri', JSON.stringify(param))
+            window.location = encodeURI(`https://www.facebook.com/dialog/oauth?client_id=${appId}&redirect_uri=${uri}&response_type=token`)
+          }
+        }
+      }
+    }
+  },
+  mounted () {
+    let vm = this
+    vm.action = vm.getParameterByName('action')
+    window.fbAsyncInit = () => {
+      FB.init({
+        appId: 'YOUR_APP_ID',
+        cookie: true,
+        xfbml: true,
+        version: 'v2.8'
+      })
+      FB.getLoginStatus(function (response) {
+        vm.statusChangeCallback(response)
+      })
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,24 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+// import router from './router'
+
+(function (d, s, id) {
+  var js
+  var fjs = d.getElementsByTagName(s)[0]
+  if (d.getElementById(id)) return
+  js = d.createElement(s); js.id = id
+  js.src = '//connect.facebook.net/en_US/sdk.js'
+  fjs.parentNode.insertBefore(js, fjs)
+}(document, 'script', 'facebook-jssdk'))
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  // router,
+  template: '<App/>',
+  components: { App }
+})
